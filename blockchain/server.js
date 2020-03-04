aff07c314ce8b45e23181ce72b93b9a87b51dfa7
Julien Sié <julien.sie@gmail.com>
2020-03-04 19:50:04 +0100

    send empty blockchain

--- blockchain/server.js
+++ blockchain/server.js
@@ -3,13 +3,16 @@
 const express = require('express');
 const morgan = require('morgan');
 const fs = require('fs');
+var crypto = require('crypto');
 
 const app = express();
 app.use(morgan("dev"));
 
 const blockchain = [];
 
-app.get("/", (req,res) => res.send(JSON.parse(fs.readFileSync('/tmp/secret-code/secret.json'))));
+JSON.parse(fs.readFileSync('/tmp/secret-code/secret.json'))
+
+app.get("/", (req,res) => res.send(blockchain));
 
 const port = process.env.PORT || 9000;
 app.listen(port, () => console.log(`NodeJS server listening on port ${port}`));
