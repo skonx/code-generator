5be2f193370ad8fbe7c34d35f65aff59b279af90
Julien Sié <julien.sie@gmail.com>
2020-03-04 11:45:35 +0100

    refactoring

---
+++ init.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+// default and base values
+const (
+	Base            = "0123456789_-.AZERTYUIOPMLKJHGFDSQWXCVBNazertyuiopmlkjhgfdsqwxcvbn"
+	DefaultCodeSize = 64
+)
+
+var codeSize int
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func controlIntEnvVar(env string, msg string, value *int) {
+	if size := os.Getenv(env); size != "" {
+		s, err := strconv.Atoi(size)
+		check(err)
+		*value = s
+	} else {
+		*value = DefaultCodeSize
+	}
+
+	fmt.Printf("- %s = %d\n", msg, *value)
+}
+
+func controlEnvs() {
+	fmt.Println("Control command line arguments and set global settings...")
+	controlIntEnvVar("CODE_SIZE", "code size", &codeSize)
+}
