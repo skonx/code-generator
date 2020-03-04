77d966ec0f385f02ee8194a3854594a30a644a36
Julien Sié <julien.sie@gmail.com>
2020-03-04 19:03:10 +0100

    read from secret file

--- blockchain/server.js
+++ blockchain/server.js
@@ -2,13 +2,14 @@
 
 const express = require('express');
 const morgan = require('morgan');
+const fs = require('fs');
 
 const app = express();
 app.use(morgan("dev"));
 
-const blockchain = []
+const blockchain = [];
 
-app.get("/", (req,res) => res.send(blockchain));
+app.get("/", (req,res) => res.send(JSON.parse(fs.readFileSync('/tmp/secret-code/secret.json'))));
 
 const port = process.env.PORT || 9000;
 app.listen(port, () => console.log(`NodeJS server listening on port ${port}`));
